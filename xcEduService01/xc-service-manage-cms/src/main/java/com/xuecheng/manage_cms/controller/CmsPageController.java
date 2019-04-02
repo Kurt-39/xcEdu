ac8e47d2ae04d45cf8e83f0d0b7e2b1219cd0436
kurt <yumao852314815@163.com>
2019-04-02 19:47:14 +0800

    #添加注解

--- xcEduService01/xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/controller/CmsPageController.java
+++ xcEduService01/xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/controller/CmsPageController.java
@@ -10,12 +10,14 @@ import com.xuecheng.manage_cms.service.PageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("/cms/page")
 public class CmsPageController implements CmsPageControllerApi {
 
     @Autowired
