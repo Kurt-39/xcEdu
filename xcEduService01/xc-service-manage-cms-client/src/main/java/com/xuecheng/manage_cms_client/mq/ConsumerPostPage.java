211c2f2553fca2b203fa891f94c4691771750170
kurt <yumao852314815@163.com>
2019-04-18 21:45:17 +0800

    ##mq消费方

--- xcEduService01/xc-service-manage-cms-client/src/main/java/com/xuecheng/manage_cms_client/mq/ConsumerPostPage.java
+++ xcEduService01/xc-service-manage-cms-client/src/main/java/com/xuecheng/manage_cms_client/mq/ConsumerPostPage.java
@@ -1,11 +1,41 @@
 package com.xuecheng.manage_cms_client.mq;
 
+import com.alibaba.fastjson.JSON;
+import com.xuecheng.framework.domain.cms.CmsPage;
+import com.xuecheng.manage_cms_client.dao.CmsPageRepository;
+import com.xuecheng.manage_cms_client.service.PageService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.Map;
 
 @Component
 public class ConsumerPostPage {
     private static final Logger LOGGER= LoggerFactory.getLogger(ConsumerPostPage.class);
+@Autowired
+    CmsPageRepository cmsPageRepository;
+@Autowired
+    PageService pageService;
+
+@RabbitListener(queues = {"${xuecheng.mq.queue}"})
+    public void postPage(String msg){
+    //解析消息
+     Map map = JSON.parseObject(msg, Map.class);
+     //得到消息中的页面
+    String pageId = (String) map.get("pageId");
+    //校验页面合法否
+    CmsPage cmsPageById = pageService.findCmsPageById(pageId);
+    if (cmsPageById==null){
+        LOGGER.error("receive postpage msg,cmsPage is null,pageId:{}",pageId);
+        return;
+    }
+    //调用service方法将页面从GridFs中下载到服务器
+    pageService.savePageToServerPath(pageId);
+}
+
+
 
 }
