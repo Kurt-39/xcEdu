d5c280363bb7a62e02c48059b0805387be458940
kurt <yumao852314815@163.com>
2019-04-23 22:55:46 +0800

    课程查询后端代码

--- xcEduService01/xc-service-api/src/main/java/com/xuecheng/api/course/CourseControllerApi.java
+++ xcEduService01/xc-service-api/src/main/java/com/xuecheng/api/course/CourseControllerApi.java
@@ -1,7 +1,14 @@
 package com.xuecheng.api.course;
 
+import com.xuecheng.framework.domain.course.Teachplan;
 import com.xuecheng.framework.domain.course.ext.TeachplanNode;
+import com.xuecheng.framework.model.response.ResponseResult;
+import io.swagger.annotations.ApiOperation;
 
 public interface CourseControllerApi {
+    @ApiOperation("查找课程列表")
     public TeachplanNode findTeachplanList(String id);
+    @ApiOperation("添加课程")
+    public ResponseResult addTeachplan(Teachplan teachplan);
+
 }
--- xcEduService01/xc-service-manage-course/src/main/java/com/xuecheng/manage_course/controller/CourseController.java
+++ xcEduService01/xc-service-manage-course/src/main/java/com/xuecheng/manage_course/controller/CourseController.java
@@ -1,13 +1,13 @@
 package com.xuecheng.manage_course.controller;
 
 import com.xuecheng.api.course.CourseControllerApi;
+import com.xuecheng.framework.domain.course.Teachplan;
 import com.xuecheng.framework.domain.course.ext.TeachplanNode;
+import com.xuecheng.framework.model.response.ResponseResult;
 import com.xuecheng.manage_course.service.CourseService;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/course")
@@ -19,5 +19,11 @@ public class CourseController implements CourseControllerApi {
     @ApiOperation("获取数据列表")
     public TeachplanNode findTeachplanList(@PathVariable("courseId")String courseId) {
         return courseService.findTeachplanList(courseId);
+    }
+
+    @Override
+    @PostMapping("/teachplan/add")
+    public ResponseResult addTeachplan(@RequestBody Teachplan teachplan) {
+        return courseService.addTeachplan(teachplan);
     }
 }
